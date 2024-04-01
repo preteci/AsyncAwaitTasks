73992d14737a360f45db2da6e46cc18d5fa8f74b
preteci <verythreatening@gmail.com>
2024-04-01 09:59:09 +0200

    Add project files.

---
+++ TASK1/Program.cs
@@ -0,0 +1,64 @@
+﻿using static System.Net.WebRequestMethods;
+using System.IO;
+using System;
+
+namespace TASK1
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string fileName = "image";
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+            string imageUrl = "https://via.placeholder.com/150/92c952";
+            await DownloadImage(imageUrl, filePath);  
+        }
+
+        public static async Task DownloadImage(string imageUrl, string filePath)
+        {
+            try
+            {
+                using(var httpClient = new HttpClient())
+                {
+                    HttpResponseMessage resposne = await httpClient.GetAsync(imageUrl);
+
+                    resposne.EnsureSuccessStatusCode();
+
+                    string contentType = resposne.Content.Headers.ContentType.MediaType;
+
+                    filePath += contentType;
+
+                    Console.Write(contentType);
+
+                    byte[] imageData = await resposne.Content.ReadAsByteArrayAsync();
+
+                    using (FileStream fs = new FileStream(filePath, FileMode.Create))
+                    {
+                        fs.Write(imageData, 0, imageData.Length);
+                    }
+
+                    Console.WriteLine($"Download complete!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error while downloading image file: {ex.Message}");
+            }
+        }
+
+        private static string GetExtensionFromMime(string mimeType)
+        {
+            switch (mimeType)
+            {
+                case "image/jpeg":
+                    return ".jpg";
+                case "image/png":
+                    return ".png";
+                case "image/gif":
+                    return ".gif";
+                default:
+                    return ".unknown";
+            }
+        }
+    }
+}
---
+++ TASK4/Program.cs
@@ -0,0 +1,108 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Octokit;
+using System;
+
+namespace TASK4
+{
+    internal class Program
+    {
+        private const string PagedIssueQuery =
+        @"query ($repo_name: String!,  $start_cursor:String) {
+          repository(owner: ""dotnet"", name: $repo_name) {
+            issues(last: 25, before: $start_cursor)
+             {
+                totalCount
+                pageInfo {
+                  hasPreviousPage
+                  startCursor
+                }
+                nodes {
+                  title
+                  number
+                  createdAt
+                }
+              }
+            }
+          }
+        ";
+
+
+        public class GraphQLRequest
+        {
+            [JsonProperty("query")]
+            public string Query { get; set; }
+
+            [JsonProperty("variables")]
+            public IDictionary<string, object> Variables { get; } = new Dictionary<string, object>();
+
+            public string ToJsonText() =>
+                JsonConvert.SerializeObject(this);
+        }
+
+        static async Task Main(string[] args)
+        {
+            string key = "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";
+
+            var client = new GitHubClient(new Octokit.ProductHeaderValue("IssueQueryDemo"))
+            {
+                Credentials = new Octokit.Credentials(key)
+            };
+
+            CancellationTokenSource cancellationSource = new CancellationTokenSource();
+
+            await foreach (var issue in GetIssuesAsync(client, PagedIssueQuery, "docs", cancellationSource.Token))
+            {
+                Console.WriteLine(issue);
+            }
+
+            Console.WriteLine("All issues fetched.");
+        }
+
+        private static async IAsyncEnumerable<JObject> GetIssuesAsync(GitHubClient client, string queryText, string repoName, CancellationToken cancel)
+        {
+            var issueAndPRQuery = new GraphQLRequest
+            {
+                Query = queryText
+            };
+
+            issueAndPRQuery.Variables["repo_name"] = repoName;
+
+            JArray finalResults = new JArray();
+            bool hasMorePages = true;
+            int pagesReturned = 0;
+            int issuesReturned = 0;
+
+            // Stop with 10 pages, because these are large repos:
+            while (hasMorePages && (pagesReturned++ < 10))
+            {
+                var postBody = issueAndPRQuery.ToJsonText();
+                var response = await client.Connection.Post<string>(new Uri("https://api.github.com/graphql"),
+                    postBody, "application/json", "application/json");
+
+                JObject results = JObject.Parse(response.HttpResponse.Body.ToString());
+
+                int totalCount = (int)issues(results)["totalCount"];
+                hasMorePages = (bool)pageInfo(results)["hasPreviousPage"];
+                issueAndPRQuery.Variables["start_cursor"] = pageInfo(results)["startCursor"].ToString();
+                issuesReturned += issues(results)["nodes"].Count();
+
+                finalResults.Merge(issues(results)["nodes"]);
+                var array = issues(results)["nodes"];
+
+                Console.WriteLine($"You have returned {issuesReturned}");
+
+                foreach (var issue in array)
+                {
+                    yield return (JObject)issue;
+                }
+
+                cancel.ThrowIfCancellationRequested();
+
+            }
+
+            JObject issues(JObject result) => (JObject)result["data"]["repository"]["issues"];
+            JObject pageInfo(JObject result) => (JObject)issues(result)["pageInfo"];
+        }
+    }
+}
